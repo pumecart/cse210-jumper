0a62a64a39176c103bd30d4d002e5ec8074a3c03
garretts8 <gar21085@byui.edu>
2022-05-24 17:30:30 -0600

    Added function to code

--- game/jumper_image.py
+++ game/jumper_image.py
@@ -1,4 +1,64 @@
 #Image printer
-#End the game
+
+class Jumper_image:
+  
+  def __init__(self):
+    self.incorrect = 0
+  
+  def print_jumper(self, incorrect):
+    if incorrect == 0:
+        print("   ___   ")
+        print("  /___\  ")
+        print("  \   /  ")
+        print("   \ /   ")
+        print("    O    ")
+        print("   /|\   ")
+        print("   / \   ")
+        print("         ")
+        print("^^^^^^^^^") 
+
+    elif incorrect == 1:
+        print("  /___\  ")
+        print("  \   /  ")
+        print("   \ /   ")
+        print("    O    ")
+        print("   /|\   ")
+        print("   / \   ")
+        print("         ")
+        print("^^^^^^^^^")
+
+    elif incorrect == 2:    
+        print("  \   /  ")
+        print("   \ /   ")
+        print("    O    ")
+        print("   /|\   ")
+        print("   / \   ")
+        print("         ")
+        print("^^^^^^^^^")
+
+    elif incorrect == 3:
+        print("   \ /   ")
+        print("    O    ")
+        print("   /|\   ")
+        print("   / \   ")
+        print("         ")
+        print("^^^^^^^^^")
+
+    elif incorrect == 4:
+        print("    O    ")
+        print("   /|\   ")
+        print("   / \   ")
+        print("         ")
+        print("^^^^^^^^^")
+
+    elif incorrect >= 5:
+        print("    X    ")
+        print("   /|\   ")
+        print("   / \   ")
+        print("         ")
+        print("^^^^^^^^^")
+        
+
+
 
 #Alexander
