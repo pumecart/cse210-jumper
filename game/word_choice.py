1c3e21fcc65d87a713e3ad1d00debabcea62788c
garretts8 <gar21085@byui.edu>
2022-05-27 17:33:17 -0600

    Corrected word_choice.py

--- game/word_choice.py
+++ game/word_choice.py
@@ -5,11 +5,12 @@ class Word:
   def __init__(self):
     self._word_list = []
     self._index = -2
+    self.guess = ""
     
-  def get_word(self):    
-    self.readFile = "words.txt"
+  def get_word(self, readFile):    
+    readFile = "C:/Users/mitch/Desktop/BYUI/Programming with Classes/Week 5 Team/cse210-jumper/game/words.txt"
     # Word bank
-    with open("words.txt") as word_lists:
+    with open(readFile) as word_lists:
       for word in word_lists:
         word = word.strip()
         # print(word)
@@ -17,7 +18,7 @@ class Word:
       # print(word_list)
     #Chooses words
     self._special_word = random.choice(self._word_list)
-    print(self._special_word)
+    return self._special_word
 
   # Set letter position
   def get_spaces(self, _special_word):
@@ -26,15 +27,15 @@ class Word:
 
   def get_letter_position(self, _special_word, _index):
     while _index != -1:
+      self.guess = input("What is your guess: ")
     # Set Blank Letters
-      guess = input("What is your guess: ")
       # i = special_word.index(guess)
       for i in range(len(_special_word)):
-        if guess == _special_word[i]:
+        if self.guess == _special_word[i]:
           # replace each index with the matching guess letter in special_word.
           # spaces[0:1] leaves "_" before the special_word[i]
           # spaces[i+1:] leaves "_" after the special_word[i]
-          self.spaces = self.spaces[0:i] + guess + self.spaces[i+1:]
+          self.spaces = self.spaces[0:i] + self.guess + self.spaces[i+1:]
       # print(display)
           print(self.spaces)
         
