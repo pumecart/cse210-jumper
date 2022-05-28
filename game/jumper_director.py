68b3f7c6676b242d725eaf32742f6890d73477ba
garretts8 <gar21085@byui.edu>
2022-05-27 19:27:22 -0600

    Updated version Team Work

--- game/jumper_director.py
+++ game/jumper_director.py
@@ -23,26 +23,34 @@ class Director:
         while self._isPlaying:
             self._get_inputs()
             self._do_updates()
-            break
-    #         self._do_outputs()
+            self._do_outputs()
     
     def _get_inputs(self):
-        # self._guess = self._Word.get_letter_guess()
-        self.guess =  self._TerminalService.read_letter(input("What is your guess: "))
+        self._guess = self._Word.get_letter_guess()
+        # self._guess =  self._TerminalService.read_letter(input("What is your guess: "))
+    
 
+    # def _do_updates(self):
+    #     self._Goal = self._Word.get_letter_position(self._Word.get_word(), self._index)
+    #     # self._Word.get_spaces(self._Goal)
+    #     # self._letter_position = self._Word.get_letter_position(self._index, self._Word.get_word("words.txt"))
+    
     def _do_updates(self):
-        self._Goal = self._Word.get_letter_position(self._Word.get_word("words.txt"), self._index)
-        # self._Word.get_spaces(self._Goal)
-        # self._letter_position = self._Word.get_letter_position(self._index, self._Word.get_word("words.txt"))
-       
-        
-    #     self._incorrect += self._Word.incorrect_guess(self._Goal, self._guess)
-    #     if self._incorrect == 5:
-    #         self._isPlaying = False
-    #     self._win = self._Word.guessed_word(self._Goal)
-    #     if self._win == True:
-    #         self._isPlaying = False
-
-    # def _do_outputs(self):
-    #     self._Word.set_letter_position(self._letter_position, self._guess)
-    #     self._JumperImage.print_jumper(self._incorrect)
+        self._letter_position = self._Word.get_letter_position(self._Goal, self._guess)
+        self._incorrect += self._Word.incorrect_guess(self._Goal, self._guess)
+        if self._incorrect == 5:
+            self._isPlaying = False
+        self._win = self._Word.guessed_word(self._Goal)
+        if self._win == True:
+            self._isPlaying = False
+            
+            
+            
+    def _do_outputs(self):
+        self._Word.set_letter_position(self._letter_position, self._guess)
+        self._JumperImage.print_jumper(self._incorrect)
+        if self._incorrect == 5:
+            self._TerminalService.write_text("Sorry, you're parachute disappeared and the Jumper has died.  Thanks for killing him.")
+        self._win = self._Word.guessed_word(self._Goal)
+        if self._win == True:
+            self._TerminalService.write_text(f"Congratulations!!! You guessed {self._Goal} correctly!!!  Thanks for keeping the Jumper alive!!!")
--- game/word_choice.py
+++ game/word_choice.py
@@ -1,11 +1,14 @@
 import random
+from terminal_service import TerminalService
 
 
 class Word:
   def __init__(self):
     self._word_list = []
     self._index = -2
-    self.guess = ""
+    self._guess = ""
+    self._letter_list = []
+    self._TerminalService = TerminalService()
     
   def get_word(self):    
     readFile = "game/words.txt"
@@ -22,23 +25,59 @@ class Word:
 
   # Set letter position
   def get_spaces(self, _special_word):
-    self.spaces = "_"*len(_special_word)
-    print(self.spaces)
+    spaces = '_'
+    for i in range(len(_special_word)):
+      self._letter_list[i] = self._letter_list.append(spaces)
 
-  def get_letter_position(self, _special_word, _index):
-    while _index != -1:
-      self.guess = input("What is your guess: ")
+  def get_letter_position(self, _special_word, guess):
     # Set Blank Letters
       # i = special_word.index(guess)
-      for i in range(len(_special_word)):
-        if self.guess == _special_word[i]:
-          # replace each index with the matching guess letter in special_word.
-          # spaces[0:1] leaves "_" before the special_word[i]
-          # spaces[i+1:] leaves "_" after the special_word[i]
-          self.spaces = self.spaces[0:i] + self.guess + self.spaces[i+1:]
-      # print(display)
-          print(self.spaces)
-        
+    for i in range(len(_special_word)):
+      if guess == _special_word[i]:
+        # replace each index with the matching guess letter in special_word.
+        # spaces[0:1] leaves "_" before the special_word[i]
+        # spaces[i+1:] leaves "_" after the special_word[i]
+        return i
+      else:
+        if guess != _special_word[i]:
+          pass
+      
+  def set_letter_position(self, i, guess):
+    for i in range(len(self._special_word)):
+      if guess == self._special_word[i]:
+        self._letter_list[i] = guess
+    print(self._letter_list)
+    
+  def incorrect_guess(self, _special_word, guess):
+    value = 0
+    for i in range(len(_special_word)):
+      if guess != _special_word[i]:
+        value += 1
+        if value == 8:
+          return 1
+      else:
+        return 0
+      
+  def get_letter_guess(self):
+    guess = self._TerminalService.read_letter("What is your guess: ")
+    return guess
+          
+  def incorrect_guess(self, _special_word, guess):
+    value = 0
+    for i in range(len(_special_word)):
+      if guess != _special_word[i]:
+        value += 1
+        if value == 8:
+          return 1
+      else:
+        return 0
+  def guessed_word(self, _special_word):
+    value = 0
+    for i in range(len(_special_word)):
+      if _special_word[i] == self._letter_list[i]:
+        value += 1
+        if value == 8:
+          return True      
 
       
     
