e53a7f4b2e5ce8c6a9d3b783db6cb1eaa2ab2a26
garretts8 <gar21085@byui.edu>
2022-05-28 13:56:06 -0600

    Final changes

--- game/jumper_main.py
+++ game/jumper_main.py
@@ -5,8 +5,3 @@ director = Director()
 director.start_game()
 
 
-# from word_choice import Word
-# word = Word()
-# word.get_word()
-# word.get_spaces(word._special_word)
-# word.get_letter_position(word._special_word, word._index)
--- game/word_choice.py
+++ game/word_choice.py
@@ -1,7 +1,6 @@
 import random
 from terminal_service import TerminalService
 
-
 class Word:
   def __init__(self):
     self._word_list = []
@@ -16,27 +15,21 @@ class Word:
     with open(readFile) as word_lists:
       for word in word_lists:
         word = word.strip()
-        # print(word)
         self._word_list.append(word)
-      # print(word_list)
     #Chooses words
     self._special_word = random.choice(self._word_list)
     return self._special_word
 
   # Set letter position
   def get_spaces(self, _special_word):
-    spaces = '_'
     for i in range(len(_special_word)):
-      self._letter_list[i] = self._letter_list.append(spaces)
+      self._letter_list.append('_ ')
+    print(*self._letter_list)
 
   def get_letter_position(self, _special_word, guess):
     # Set Blank Letters
-      # i = special_word.index(guess)
     for i in range(len(_special_word)):
       if guess == _special_word[i]:
-        # replace each index with the matching guess letter in special_word.
-        # spaces[0:1] leaves "_" before the special_word[i]
-        # spaces[i+1:] leaves "_" after the special_word[i]
         return i
       else:
         if guess != _special_word[i]:
@@ -46,7 +39,9 @@ class Word:
     for i in range(len(self._special_word)):
       if guess == self._special_word[i]:
         self._letter_list[i] = guess
-    print(self._letter_list)
+    print()
+    print(*self._letter_list)
+    print()
     
   def incorrect_guess(self, _special_word, guess):
     value = 0
@@ -71,6 +66,7 @@ class Word:
           return 1
       else:
         return 0
+      
   def guessed_word(self, _special_word):
     value = 0
     for i in range(len(_special_word)):
@@ -78,8 +74,5 @@ class Word:
         value += 1
         if value == 8:
           return True      
-
-      
-    
 
 #Mitchelle
